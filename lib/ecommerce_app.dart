c2b47676c083e0052414aa2e9f58906706bafb73
AmmarEmad-2004 <za0662003@email.com>
2025-08-15 18:46:01 +0300

    make login screen

--- lib/ecommerce_app.dart
+++ lib/ecommerce_app.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class EcommerceApp extends StatelessWidget {
@@ -6,7 +7,18 @@ class EcommerceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'E-commerce',
+      debugShowCheckedModeBanner: false,
+      color: AppColors.kScaffoldColor,
+      home: LoginScreen(),
     );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold());
   }
 }
