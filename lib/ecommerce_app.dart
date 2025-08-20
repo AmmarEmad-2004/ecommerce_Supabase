60db393e183ccb995b32d88fe3b0190c43ae0783
AmmarEmad-2004 <za0662003@email.com>
2025-08-20 17:12:21 +0300

    add bloc and go router packaegs

--- lib/ecommerce_app.dart
+++ lib/ecommerce_app.dart
@@ -1,6 +1,5 @@
 import 'package:ecommerce_app/core/theme/app_colors.dart';
 import 'package:ecommerce_app/modules/auth/presentation/screens/forget_password_screen.dart';
-import 'package:ecommerce_app/modules/auth/presentation/screens/sign_up_screen.dart';
 import 'package:flutter/material.dart';
 
 class EcommerceApp extends StatelessWidget {
