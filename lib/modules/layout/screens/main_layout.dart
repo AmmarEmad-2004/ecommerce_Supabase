62bbd1ad89355d1592a39edabcd566cc036234ca
AmmarEmad-2004 <za0662003@email.com>
2025-08-20 18:38:06 +0300

    feat: integrate NavBarCubit into MainLayout and CustomNavBar for navigation state management

--- lib/modules/layout/screens/main_layout.dart
+++ lib/modules/layout/screens/main_layout.dart
@@ -1,15 +1,16 @@
+import 'package:ecommerce_app/modules/layout/logic/nav_bar_cubit/nav_bar_cubit.dart';
 import 'package:ecommerce_app/modules/layout/widgets/custom_nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainLayout extends StatelessWidget {
   const MainLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-      bottomNavigationBar: CustomNavBar(),
+    return BlocProvider(
+      create: (context) => NavBarCubit(),
+      child: Scaffold(bottomNavigationBar: CustomNavBar()),
     );
   }
 }
-
--- lib/modules/layout/widgets/custom_nav_bar.dart
+++ lib/modules/layout/widgets/custom_nav_bar.dart
@@ -1,17 +1,33 @@
+import 'package:ecommerce_app/core/theme/app_colors.dart';
+import 'package:ecommerce_app/modules/layout/logic/nav_bar_cubit/nav_bar_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CustomNavBar extends StatelessWidget {
   const CustomNavBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      items: [
-        navBarItem(icon: Icons.home, label: 'Home'),
-        navBarItem(icon: Icons.store, label: 'Store'),
-        navBarItem(icon: Icons.favorite, label: 'Favorites'),
-        navBarItem(icon: Icons.person, label: 'Profile'),
-      ],
+    List<BottomNavigationBarItem> items = [
+      navBarItem(icon: Icons.home, label: 'Home'),
+      navBarItem(icon: Icons.store, label: 'Store'),
+      navBarItem(icon: Icons.favorite, label: 'Favorites'),
+      navBarItem(icon: Icons.person, label: 'Profile'),
+    ];
+    return BlocBuilder<NavBarCubit, NavBarState>(
+      builder: (context, state) {
+        return BottomNavigationBar(
+          elevation: 3,
+          backgroundColor: AppColors.kWhiteColor,
+          selectedItemColor: AppColors.kPrimaryColor,
+          unselectedItemColor: AppColors.kGreyColor,
+          onTap: (value) {
+            context.read<NavBarCubit>().changeIndex(value);
+          },
+          currentIndex: state.index,
+          items: items,
+        );
+      },
     );
   }
 
