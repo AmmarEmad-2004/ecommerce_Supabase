f278e4c4243b25b52af822555771bd4ec9abd66c
AmmarEmad-2004 <za0662003@email.com>
2025-08-20 17:28:14 +0300

    implement go router package and add assets folder

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,21 @@
+import 'package:ecommerce_app/core/routing/app_routers.dart';
+import 'package:ecommerce_app/modules/auth/presentation/screens/forget_password_screen.dart';
+import 'package:ecommerce_app/modules/auth/presentation/screens/login_screen.dart';
+import 'package:ecommerce_app/modules/auth/presentation/screens/sign_up_screen.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  static final router = GoRouter(
+    routes: [
+      GoRoute(path: '/', builder: (context, state) => const LoginScreen()),
+      GoRoute(
+        path: AppRouters.signUp,
+        builder: (context, state) => const SignUpScreen(),
+      ),
+      GoRoute(
+        path: AppRouters.forgetPassword,
+        builder: (context, state) => const ForgetPasswordScreen(),
+      ),
+    ],
+  );
+}
---
+++ lib/core/routing/app_routers.dart
@@ -0,0 +1,5 @@
+abstract class AppRouters {
+  static const String logIn = '/LogInScreen';
+  static const String signUp = '/SignUpScreen';
+  static const String forgetPassword = '/ForgetPasswordScreen';
+}
