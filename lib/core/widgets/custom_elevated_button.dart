29cc129efd6b2dc067bac97574c077b34c692545
Ammar Emad <za0663008@gmail.com>
2025-09-10 21:15:40 +0300

    refactor : make the radius more dynamic to reuse it

--- lib/core/widgets/custom_elevated_button.dart
+++ lib/core/widgets/custom_elevated_button.dart
@@ -2,13 +2,22 @@ import 'package:ecommerce_app/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomElevatedButton extends StatelessWidget {
-  const CustomElevatedButton({super.key, this.onPressed, required this.data});
+  const CustomElevatedButton({
+    super.key,
+    this.onPressed,
+    required this.data,
+    this.borderRadius = BorderRadius.zero,
+  });
   final Function()? onPressed;
   final String data;
+  final BorderRadiusGeometry borderRadius;
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      style: ElevatedButton.styleFrom(backgroundColor: AppColors.kPrimaryColor),
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: borderRadius),
+        backgroundColor: AppColors.kPrimaryColor,
+      ),
       onPressed: onPressed,
       child: Text(
         data,
