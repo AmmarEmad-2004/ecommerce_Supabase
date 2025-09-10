aa8236061eff831cba56a7dd7a5af82056144afa
Ammar Emad <za0663008@gmail.com>
2025-09-11 01:49:25 +0300

    finish profile view

--- lib/modules/favorite/presentation/screens/favorite_screen.dart
+++ lib/modules/favorite/presentation/screens/favorite_screen.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce_app/core/helpers/spaces.dart';
+import 'package:ecommerce_app/modules/home/presentation/widgets/product_list_view.dart';
 import 'package:flutter/material.dart';
 
 class FavoriteScreen extends StatelessWidget {
@@ -5,10 +7,24 @@ class FavoriteScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text('favorite'),
-      ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            verticalSpace(15),
+
+            Text(
+              'Your Favorite Products',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            ),
+
+            verticalSpace(15),
+            ProductsListView(),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/modules/profile/presentation/screens/profile_screen.dart
+++ lib/modules/profile/presentation/screens/profile_screen.dart
@@ -1,3 +1,6 @@
+import 'package:ecommerce_app/core/helpers/spaces.dart';
+import 'package:ecommerce_app/core/theme/app_colors.dart';
+import 'package:ecommerce_app/modules/profile/presentation/widgets/custom_profile_icons.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -5,6 +8,44 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [Text('Profile')]);
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 80),
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 35,
+                  backgroundColor: AppColors.kPrimaryColor,
+                  child: Icon(
+                    Icons.person,
+                    color: AppColors.kWhiteColor,
+                    size: 50,
+                  ),
+                ),
+                Text(
+                  "Ammar Emad",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                Text("Ammarzewain@gmail.com", style: TextStyle(fontSize: 20)),
+                verticalSpace(20),
+                CustomProfileIcons(title: 'Edit Name', icon: Icons.person),
+                verticalSpace(8),
+                CustomProfileIcons(
+                  title: 'My Orders',
+                  icon: Icons.shopping_basket,
+                ),
+                verticalSpace(8),
+                CustomProfileIcons(title: 'Logout', icon: Icons.logout),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
+
+
