ba3ffd86a65a52103d7ceb51e8d900c7f60d4618
Ammar Emad <za0663008@gmail.com>
2025-09-10 16:57:20 +0300

    feat:create category item model

---
+++ lib/modules/home/data/models/category_item_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class CategoryItemModel {
+  final IconData icon;
+  final String title;
+
+ const CategoryItemModel({required this.icon, required this.title});
+}
