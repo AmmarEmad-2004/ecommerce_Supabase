07b2423ade24f499d6b33ecd602bf5d020a41f25
Ammar Emad <za0663008@gmail.com>
2025-09-12 01:37:01 +0300

    refactor: make the shrinkwrap and physics more reusable

--- lib/modules/home/presentation/widgets/product_list_view.dart
+++ lib/modules/home/presentation/widgets/product_list_view.dart
@@ -2,13 +2,14 @@ import 'package:ecommerce_app/modules/home/presentation/widgets/custom_product_c
 import 'package:flutter/material.dart';
 
 class ProductsListView extends StatelessWidget {
-  const ProductsListView({super.key});
-
+  const ProductsListView({super.key, this.shrinkWrap, this.physics});
+  final bool? shrinkWrap;
+   final ScrollPhysics? physics;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap:shrinkWrap?? true,
+      physics:physics?? NeverScrollableScrollPhysics(),
       itemCount: 6,
       itemBuilder: (context, index) {
         return CustomProductCard();
