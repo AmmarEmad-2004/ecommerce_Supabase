49dde3847cdb5313013fac21b7a6a232957d17c9
Ammar Emad <za0663008@gmail.com>
2025-09-12 02:07:11 +0300

    make the product card navigate to product screen

--- lib/modules/home/presentation/widgets/custom_product_card.dart
+++ lib/modules/home/presentation/widgets/custom_product_card.dart
@@ -1,68 +1,75 @@
+import 'package:ecommerce_app/core/routing/app_routers.dart';
 import 'package:ecommerce_app/core/theme/app_colors.dart';
 import 'package:ecommerce_app/core/widgets/custom_elevated_button.dart';
 import 'package:ecommerce_app/modules/home/presentation/widgets/custom_product_image.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomProductCard extends StatelessWidget {
   const CustomProductCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-      ),
-      color: Color(0xfff7f2fa),
-      child: Column(
-        children: [
-          CustomProductImage(),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Product Name',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.favorite, color: AppColors.kGreyColor),
-                ),
-              ],
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouters.productDetails);
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        color: Color(0xfff7f2fa),
+        child: Column(
+          children: [
+            CustomProductImage(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Product Name',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.favorite, color: AppColors.kGreyColor),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 15, left: 15, bottom: 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      '233 LE',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      '290 LE',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        color: AppColors.kGreyColor,
-                        decoration: TextDecoration.lineThrough,
+            Padding(
+              padding: const EdgeInsets.only(right: 15, left: 15, bottom: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        '233 LE',
+                        style: TextStyle(fontWeight: FontWeight.bold),
                       ),
-                    ),
-                  ],
-                ),
-                CustomElevatedButton(
-                  borderRadius: BorderRadius.circular(8),
-                  fontSize: 16,
-                  data: "Buy Now",
-                  onPressed: () {},
-                ),
-              ],
+                      Text(
+                        '290 LE',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: AppColors.kGreyColor,
+                          decoration: TextDecoration.lineThrough,
+                        ),
+                      ),
+                    ],
+                  ),
+                  CustomElevatedButton(
+                    borderRadius: BorderRadius.circular(8),
+                    fontSize: 16,
+                    data: "Buy Now",
+                    onPressed: () {},
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
